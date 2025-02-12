695e4b9d129d62b6b43ad73930fa80e98c24b36f
EDERW <ederw267@gmail.com>
2025-02-12 12:39:33 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,18 +3,14 @@ document.getElementById("boton-inicio").addEventListener("click", function() {
     this.classList.add("oculto");
     document.getElementById("pregunta").classList.remove("oculto");
 
-    
-
-    for (let i=0; i <15; i++ ) {
+  
+    for (let i = 0; i < 15; i++) {
         crearPetalo();
-    }  
-    
-    
-    setInterval(crearPetalo,500);
+    }
+
+  
+    setInterval(crearPetalo, 500);
 });
-
-
-
 
 function mostrarRespuesta() {
     document.getElementById("pregunta").classList.add("oculto");
@@ -26,21 +22,18 @@ function crearPetalo() {
     let petalo = document.createElement("div");
     petalo.classList.add("petalos");
     document.body.appendChild(petalo);
-
-
-    let startPosX = Math.random() * window.innerWidth;
-    let startPosY = -50;
-    let startPosDelay = Math.random() *5;
-    let duration =Math.random() *3 + 3;
-
-
-    petalo.style.left = startPosX + "px";
-    petalo.style.top = startPosY +"px";
-    petalo.style.animationDuration = duration + "5";
-    petalo.style.animationDelay = startPosDelay + "5";
-
     
-    setTimeout(() =>{
-        petalo.remove();
-    }, (duration + startPosDelay) * 1000);
+    let startPosX = Math.random() * window.innerWidth; 
+    let startPosY = -50;
+    let startDelay = Math.random() * 5;
+    let duration = Math.random() * 3 + 3;
+    
+    petalo.style.left = startPosX + "px"; 
+    petalo.style.top = startPosY + "px";
+    petalo.style.animationDuration = duration + "s"; 
+    petalo.style.animationDelay = startDelay + "s"; 
+
+    setTimeout(() => {
+        petalo.remove(); 
+    }, (duration + startDelay) * 1000); 
 }
